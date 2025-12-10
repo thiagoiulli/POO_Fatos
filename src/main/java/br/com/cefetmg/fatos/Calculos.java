43697dcf2e730915ef14334157887dba2f647227
Thiago <tiullybs@gmail.com>
2025-12-09 23:03:06 -0300

    faltando lidar com sinal negativo e teclado

--- src/main/java/br/com/cefetmg/fatos/Calculos.java
+++ src/main/java/br/com/cefetmg/fatos/Calculos.java
@@ -1,20 +1,98 @@
 package br.com.cefetmg.fatos;
 
+import java.util.Random;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+
 public class Calculos {
+    private int pontuacao;
     private int num1;
     private int num2;
     private char op;
+    private String campo;
+    Random rand;
 
-
-    public int getNum1() {
-        return num1;
+    public Calculos(Label l1, Label l2, Label o1, Label c1, Button b1){
+        pontuacao = 0;
+        rand = new Random();
+        novojogo(l1, l2, o1, c1, b1);
     }
 
-    public int getNum2() {
-        return num2;
+    private char operator(int n){
+        switch (n){
+            case 1:
+                return '+';
+            case 2:
+                return '-';
+            case 3:
+                return '*';
+            case 4:
+                return '/';
+            default:
+                return '^'; //talvez eu mude isso
+        }
     }
 
-    public char getOp() {
-        return op;
+    public void atualizarcampo(int n, Label l, Button b1){
+        removeborder(b1);
+        campo = campo + n;
+        l.setText(campo);
+    }
+
+    public void tentar(Label pont, Label l1, Label l2, Label o1, Label r1, Button b1){
+        int resultado = -1;
+        switch (op){
+            case '+':
+                resultado = num1 + num2;
+                break;
+            case '-':
+                resultado = num1 - num2;
+                break;
+            case '*':
+                resultado = num1 * num2;
+                break;
+            case '/':
+                resultado = num1 / num2;
+                break;
+        }
+        if (resultado == Integer.parseInt(campo)){
+            atualizarpontuacao(pont);
+            novojogo(l1, l2, o1, r1, b1);
+        }
+        else{
+            b1.setStyle("-fx-border-color: #FF0000; -fx-border-width: 2;");
+        }
+    }
+
+    public void novojogo(Label l1, Label l2, Label o1, Label r1, Button b1) {
+        removeborder(b1);
+        num1 = rand.nextInt(90) + 10;
+        l1.setText(String.valueOf(num1));
+        num2 = rand.nextInt(90) + 10;
+        l2.setText(String.valueOf(num2));
+        op = operator(rand.nextInt(4) + 1);
+        o1.setText(String.valueOf(op));
+        r1.setText("_");
+        campo = "";
+    }
+    public void atualizarpontuacao(Label pont){
+        pontuacao++;
+        pont.setText("Voce tem " + pontuacao + " pontos!");
+    }
+
+    public void zerarjogo(Label l1, Label l2, Label o1, Label r1, Label pont, Button b1){
+        novojogo(l1, l2, o1, r1, b1);
+        pontuacao = -1;
+        atualizarpontuacao(pont);
+    }
+
+    public void deletar(Label l, Button b1){
+        removeborder(b1);
+        campo = campo.substring(0, campo.length()-1);
+        l.setText(campo);
+    }
+
+    private void removeborder(Button b1){
+        b1.setStyle("-fx-border-width: 0;");
     }
 }
--- src/main/java/br/com/cefetmg/fatos/HelloController.java
+++ src/main/java/br/com/cefetmg/fatos/HelloController.java
@@ -1,14 +1,59 @@
 package br.com.cefetmg.fatos;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label labelresultado;
+    @FXML
+    private Label labelnum1;
+    @FXML
+    private Label labelnum2;
+    @FXML
+    private Label labelop;
+    @FXML
+    private Label labelPont;
+    @FXML
+    private Button tentarbutton;
+
+    private Calculos calc;
+
+    @FXML
+    public void initialize(){
+        calc = new Calculos(labelnum1, labelnum2, labelop, labelresultado, tentarbutton);
+    }
 
     @FXML
     protected void onExitButtonClick(){
         System.exit(0);
+    }
+
+    @FXML
+    protected void numpadButtonsClick(ActionEvent e){
+        int value = Integer.parseInt(((Button) e.getSource()).getText());
+        calc.atualizarcampo(value, labelresultado, tentarbutton);
+    }
+
+    @FXML
+    protected void tentarButtonClick(){
+        calc.tentar(labelPont, labelnum1, labelnum2, labelop, labelresultado, tentarbutton);
+    }
+
+    @FXML
+    protected void passarButtonClick(){
+        calc.novojogo(labelnum1, labelnum2, labelop, labelresultado, tentarbutton);
+    }
+
+    @FXML
+    protected void reiniciarButtonClick(){
+        calc.zerarjogo(labelnum1, labelnum2, labelop, labelresultado, labelPont, tentarbutton);
+    }
+
+    @FXML
+    protected void backspaceButtonClick(){
+        calc.deletar(labelresultado, tentarbutton);
     }
 }
